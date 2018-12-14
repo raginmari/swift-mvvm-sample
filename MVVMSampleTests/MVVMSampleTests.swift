350d098d7f6c82f6602acc73d56aa88fc6e09f5a
Reimar Twelker <twelker@adesso-mobile.de>
2018-12-14 13:48:18 +0100

    Initial Commit

---
+++ MVVMSampleTests/MVVMSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVMSampleTests.swift
+//  MVVMSampleTests
+//
+//  Created by Reimar Twelker on 14.12.18.
+//  Copyright © 2018 adesso mobile solutions GmbH. All rights reserved.
+//
+
+import XCTest
+@testable import MVVMSample
+
+class MVVMSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
